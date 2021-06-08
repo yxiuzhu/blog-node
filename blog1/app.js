e3cd9fbbdcc50959d2fff2c0c454d68e44e7e991
yxiuzhu <y_xiuzhu@163.com>
2021-06-08 08:46:40 +0800

    cookie

--- blog1/app.js
+++ blog1/app.js
@@ -3,6 +3,17 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  console.log('d.toGMTString() is ', d.toGMTString())
+  return d.toGMTString
+}
+
+// session 数据
+const SESSION_DATA = {}
+
 // 用于处理 post data
 const getPostData = (req) => {
   const promise = new Promise((resolve, reject) => {
@@ -45,6 +56,35 @@ const serverHandle = (req, res) => {
   // 解析query
   req.query = querystring.parse(url.split('?')[1])
 
+  // 解析 cookie
+  req.cookie = {}
+  // 获取cookie：req.headers.cookie
+  const cookieStr = req.headers.cookie || ''  //k1=v1;k2=v2;
+  cookieStr.split(';').forEach(item => {
+    if (!item) {
+      return
+    }
+    const arr = item.split('=')
+    const key = arr[0].trim()
+    const val = arr[1].trim()
+    req.cookie[key] = val
+  })
+  console.log('req.cookie is ', req.cookie)
+
+  // 解析 session
+  let needSetCookie = false
+  let userId = req.cookie.userid
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {}
+    }
+  } else {
+    needSetCookie = true
+    userId = `${Date.now()}_${Math.random()}`
+    SESSION_DATA[userId] = {}
+  }
+  req.session = SESSION_DATA[userId]
+
   // 处理 post data
   getPostData(req).then(postData => {
     req.body = postData
@@ -60,6 +100,10 @@ const serverHandle = (req, res) => {
     const blogResult = handleBlogRouter(req, res)
     if (blogResult) {
       blogResult.then(blogData => {
+        if (needSetCookie) {
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
+
         res.end(
           JSON.stringify(blogData)
         )
@@ -71,6 +115,10 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res)
     if (userResult) {
       userResult.then(userData => {
+        if (needSetCookie) {
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
+
         res.end(
           JSON.stringify(userData)
         )
--- blog1/src/router/user.js
+++ blog1/src/router/user.js
@@ -1,20 +1,52 @@
-const { loginCheck } = require('../controller/user')
+const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  console.log('d.toGMTString() is ', d.toGMTString())
+  return d.toGMTString
+}
 
 const handleUserRouter = (req, res) => {
   const method = req.method  //GET / POST
 
   // 登录接口
-  if (method === 'POST' && req.path === '/api/user/login') {
-    const { username, password } = req.body
-    const result = loginCheck(username, password)
+  if (method === 'GET' && req.path === '/api/user/login') {
+    // const { username, password } = req.body
+    const { username, password } = req.query
+    const result = login(username, password)
     
     return result.then(data => {
       if (data.username) {
+        // 设置 session
+        req.session.username = data.username
+        req.session.realname = data.realname
+
+        console.log('req.session is ', req.session)
+
+        // 操作 cookie
+        // httpOnly只允许后端修改，不允许前端修改
+        // res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
         return new SuccessModel()
       }
       return new ErrorModel('登录失败')
     })
+  }
+
+  // 登录验证测试
+  if (method === 'GET' && req.path === '/api/user/login-test') {
+    if (req.session.username) {
+      return Promise.resolve(
+        new SuccessModel({
+          session: req.session
+        })
+      )
+    }
+    return Promise.resolve(
+      new ErrorModel('尚未登录')
+    )
   }
 }
 
