1d35d52c9c19cee4484133b8dc83e2aff5f758ca
yxiuzhu <y_xiuzhu@163.com>
2021-06-18 15:08:53 +0800

    router

--- routes/blog.js
+++ routes/blog.js
@@ -8,9 +8,9 @@ const {
   updateBlog,
   delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
-/* GET home page. */
-router.get('/list', function (req, res, next) {
+router.get('/list', (req, res, next) => {
   let author = req.query.author || ''
   let keyword = req.query.keyword || ''
 
@@ -35,11 +35,56 @@ router.get('/list', function (req, res, next) {
   })
 });
 
-router.get('/detail', function (req, res, next) {
-  res.json({
-    errno: 0,
-    data: 'OK'
+router.get('/detail', (req, res, next) => {
+  const result = getDetail(req.query.id)
+  return result.then(data => {
+    res.json(
+      new SuccessModel(data)
+    )
   })
 });
+
+// 使用loginCheck中间件来验证登录
+router.post('/new', loginCheck, (req, res, next) => {
+  // req.body.author = 'cuihua' //假数据，待开发登录时再改成真实数据
+  req.body.author = req.session.username
+  const result = newBlog(req.body)
+  return result.then(data => {
+    res.json(
+      new SuccessModel(data)
+    )
+  })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+  const result = updateBlog(req.query.id, req.body)
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel()
+      )
+    } else {
+      res.json(
+        new ErrorModel('更新博客失败')
+      )
+    }
+  })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+  const author = req.session.username
+  const result = delBlog(req.query.id, author)
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel()
+      )
+    } else {
+      res.json(
+        new ErrorModel('删除博客失败')
+      )
+    }
+  })
+})
 
 module.exports = router;
